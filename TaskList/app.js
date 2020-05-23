c98548b599f263f8e10d89c4120a71057bf42a54
ToprakOzgur <zgrtprk@gmail.com>
2020-05-23 16:23:26 +0300

    remove, filter, clear task added

--- TaskList/app.js
+++ TaskList/app.js
@@ -11,6 +11,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task events
+  clearBtn.addEventListener('click', clearTasks)
+  // Filter task event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -37,9 +43,45 @@ function addTask(e) {
 
   //Apppend li to ul
   taskList.appendChild(li);
+  console.log(li);
 
   // Clear Input
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Sure ?')) {
+
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks 
+function clearTasks(e) {
+  // taskList.innerHTML = '';
+
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(
+    function (task) {
+      const item = task.firstChild.textContent;
+      if (item.toLowerCase().indexOf(text) != -1) {
+        task.style.display = 'block';
+      } else {
+        task.style.display = 'none';
+      }
+    }
+  );
 }
