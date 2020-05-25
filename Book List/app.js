e6b02851bc53f371ba69c11bb82f74821ea42b32
ToprakOzgur <zgrtprk@gmail.com>
2020-05-25 11:29:58 +0300

    book list refactored to ES6

--- Book List/app.js
+++ Book List/app.js
@@ -1,61 +1,62 @@
 // Book Constructor
-function Book(title, author, isbn) {
-  this.title = title;
-  this.author = author;
-  this.isbn = isbn;
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
 }
 
 //UI Constructor
-function UI() {}
+class UI {
+  constructor() {}
 
-UI.prototype.addBookToList = function (book) {
-  const list = document.getElementById('book-list');
-  //Create tr element
-  const row = document.createElement('tr');
-  //insert cols
-  row.innerHTML = `
+  addBookToList(book) {
+    const list = document.getElementById('book-list');
+    //Create tr element
+    const row = document.createElement('tr');
+    //insert cols
+    row.innerHTML = `
   <td>${book.title}</td>
   <td>${book.author}</td>
   <td>${book.isbn}</td>
   <td><a href="#" class="delete">X</a></td>
   `;
-
-  list.appendChild(row);
-}
-// Show Alerts
-UI.prototype.showAlert = function (message, clasName) {
-  // Create div
-  const div = document.createElement('div');
-  // Add classes
-  div.className = `alert ${clasName}`;
-  // Add text
-  div.appendChild(document.createTextNode(message));
-  // Get parent
-  const container = document.querySelector('.container');
-  // Get form
-  const form = document.querySelector('#book-form');
-  // Insert alert
-  container.insertBefore(div, form);
-
-  // Timeout after 3 sec
-  setTimeout(function () {
-    document.querySelector('.alert').remove();
-  }, 3000);
-}
-
-// Delete Book
-UI.prototype.deleteBook = function (target) {
-  if (target.clasName === 'delete') {
-    target.parentElement.parentElement.remove();
+    list.appendChild(row);
+  }
+  // Show Alerts
+  showAlert(message, clasName) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${clasName}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    // Get form
+    const form = document.querySelector('#book-form');
+    // Insert alert
+    container.insertBefore(div, form);
+    // Timeout after 3 sec
+    setTimeout(function () {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+  // Delete Book
+  deleteBook(target) {
+    if (target.clasName === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
+  }
+  // Clear fields
+  clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
   }
 }
 
-// Clear fields
-UI.prototype.clearFields = function () {
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('isbn').value = '';
-}
 
 // Event Listeners add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
