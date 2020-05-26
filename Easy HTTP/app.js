7b1fc7415754c6e1bac79e963520f728db15eef8
ToprakOzgur <zgrtprk@gmail.com>
2020-05-26 13:47:24 +0300

    easy http with Ajax and ES5 protoypes

---
+++ Easy HTTP/app.js
@@ -0,0 +1,39 @@
+const http = new easyHTTP;
+
+// Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts',
+//   function (err, posts) {
+//     err ? console.log(err) : console.log(posts);
+//   }
+// );
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1',
+//   function (err, post) {
+//     err ? console.log(err) : console.log(post);
+//   }
+// );
+
+// Create Data
+const data = {
+  title: "Custom Post",
+  body: "This is a custom post"
+};
+
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, post) {
+//   err ? console.log(err) : console.log(post);
+// });
+
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (err, post) {
+//   err ? console.log(err) : console.log(post);
+// });
+
+// Delete Post
+// http.delete('https://jsonplaceholder.typicode.com/posts/1',
+//   function (err, reponse) {
+//     err ? console.log(err) : console.log(reponse);
+//   }
+// );
