33669f49ef9325458907e8af1f051d139421f766
ToprakOzgur <zgrtprk@gmail.com>
2020-06-13 10:12:27 +0300

    ecommerce repo class

---
+++ ECommerce/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRepo = require("./repositories/users");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send(`
+  <div>
+    <form method="POST">
+      <input name="email" placeholder="email"/> 
+      <input name="password" placeholder="password"/> 
+      <input name="passwordConfirmation" placeholder="password confirmation"/>
+      <button>Sign Up</button> 
+    </form>
+  </div>
+  `);
+});
+
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+
+  if (existingUser) {
+    return res.send("email in use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must match");
+  }
+
+  res.send("Account Created");
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+});
