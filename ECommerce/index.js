c3b90a8e12a21e253c1c5a1fe8264480b3aa41a8
ToprakOzgur <zgrtprk@gmail.com>
2020-06-14 07:51:50 +0300

    ecoomerce auth

--- ECommerce/index.js
+++ ECommerce/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const usersRepo = require("./repositories/users");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["fdksjfksldfklfkasjfdkdsfkl"],
+  })
+);
 
-app.get("/", (req, res) => {
+app.get("/signup", (req, res) => {
   res.send(`
   <div>
+  Your id is: ${req.session.userId}
     <form method="POST">
       <input name="email" placeholder="email"/> 
       <input name="password" placeholder="password"/> 
@@ -19,7 +26,7 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
 
   const existingUser = await usersRepo.getOneBy({ email });
@@ -32,7 +39,53 @@ app.post("/", async (req, res) => {
     return res.send("Passwords must match");
   }
 
+  // Create a user in our user repo  to represent this person
+  const user = await usersRepo.create({ email, password });
+
+  // Store the id of that user inside the users cookie
+  req.session.userId = user.id;
+
   res.send("Account Created");
+});
+
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("You are signed out!");
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`
+    <div>
+    <form method="POST">
+      <input name="email" placeholder="email"/> 
+      <input name="password" placeholder="password"/> 
+      <button>Sign In</button> 
+    </form>
+  </div>
+  `);
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send("Email not found");
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+
+  if (!validPassword) {
+    return res.send("Invalid password");
+  }
+
+  req.session.userId = user.id;
+
+  res.send("You are signed in!!!");
 });
 
 app.listen(3000, () => {
--- ECommerce/repositories/users.js
+++ ECommerce/repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
   constructor(filename) {
@@ -25,11 +28,35 @@ class UsersRepository {
 
   async create(attrs) {
     attrs.id = this.randomId();
+
+    //salt and hash
+    const salt = crypto.randomBytes(8).toString("hex");
+    const buf = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
+    const record = {
+      ...attrs,
+      password: `${buf.toString("hex")}.${salt}`,
+    };
+
+    records.push(record);
 
     //write updated records array back to file
     await this.writeAll(records);
+
+    return record;
+  }
+
+  async comparePasswords(saved, supplied) {
+    // const result = saved.split(".");
+    // const hashed = result[0];
+    // const salt = result[1];
+
+    //same as above 3 lines(array destructring)
+    const [hashed, salt] = saved.split(".");
+    const hashedSupplied = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSupplied.toString("hex");
   }
 
   async writeAll(records) {
